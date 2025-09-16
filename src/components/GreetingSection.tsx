713deb40a024d64f3f60ed94dad689eafdd9a06a
kawarakawara <2201291s.asojuku.ac.jp@gmail.com>
2025-09-16 10:05:36 +0900

    挨拶の名前の変更

--- src/components/GreetingSection.tsx
+++ src/components/GreetingSection.tsx
@@ -32,7 +32,7 @@ export default function GreetingSection() {
                     流山市アマチュアゴルフ協会
                   </p>
                   <p className="text-green-700 font-medium text-center">
-                        齋藤浩継
+                        秋元真一
                   </p>
                 </div>
               </div>
